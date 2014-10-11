3a99c27a3f3b35991a177a06c640da2215420200
Harald Lapp <harald@octris.org>
2014-10-12 00:24:30 +0200

    use relativ path from current file instead of relative path from include_path

--- libs/tpl/parser/html.class.php
+++ libs/tpl/parser/html.class.php
@@ -10,8 +10,8 @@
  */
 
 namespace org\octris\core\tpl\parser {
-    require_once('org.octris.core/tpl.class.php');
-    require_once('org.octris.core/tpl/parser.class.php');
+    require_once('../../tpl.class.php');
+    require_once('../parser.class.php');
 
     /**
      * HTML Parser for auto-escaping functionality.
