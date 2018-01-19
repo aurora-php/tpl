c781115c4139f500e99541d52f94851b03f7eb96
Harald Lapp <h.lapp@clipdealer.de>
2018-01-19 17:33:10 +0100

    bugfix: reflection didn't work for all kinds of callables

--- libs/Tpl/Extension/AbstractExtension.php
+++ libs/Tpl/Extension/AbstractExtension.php
@@ -65,6 +65,20 @@ abstract class AbstractExtension
     }
 
     /**
+     * Return instance for reflection of callable.
+     * 
+     * @return  \ReflectionFunctionAbstract
+     */
+    final protected function getReflectionCallable()
+    {
+        return (is_array($this->fn) 
+                    ? new ReflectionMethod($this->fn[0], $this->fn[1]) 
+                    : (is_object($this->fn) && is_callable($this->fn, '__invoke')
+                        ? new ReflectionMethod($this->fn, '__invoke')
+                        : new ReflectionFunction($this->fn)));
+    }
+
+    /**
      * Return min,max number of parameters.
      *
      * @return  array
@@ -74,7 +88,7 @@ abstract class AbstractExtension
         static $ret = null;
         
         if (is_null($ret)) {
-            $ref = new ReflectionFunction($this->fn);
+            $ref = $this->getReflectionCallable();
 
             $min = $ref->getNumberOfRequiredParameters();
             $max = ($ref->isVariadic() ? -1 : $ref->getNumberOfParameters());
