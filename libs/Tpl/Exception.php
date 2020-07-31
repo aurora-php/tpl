550431dc0207169365be0993dabba36fd65f9ac9
Harald Lapp <harald@octris.org>
2020-07-31 16:11:07 +0200

    initial commit standard template exception

---
+++ libs/Tpl/Exception.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the 'octris/tpl' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Tpl;
+
+/**
+ * Compiler exception.
+ *
+ * @copyright   copyright (c) 2020-present by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+class Exception extends \Exception
+{
+}
